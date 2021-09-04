c0cc30212d2a557b3b7703e1b6fd2e1e716b1da6
Sluchaynaya Kotya <sluchaynayakotya@gmail.com>
2021-09-04 22:10:53 +0300

    client routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import './index.css'
-import RickAndMorty from './RickAndMorty/RickAndMorty'
+
 
 
 ReactDOM.render ( <React.StrictMode>
-                    <RickAndMorty />
+                    <BrowserRouter basename="/react"> {/* same as package.json -> homepage */}
+                      <Suspense fallback={ <div>Loading...</div> }>
+                        <Switch>
+
+                          <Route exact path="/">
+                            <div className="container">
+                              <div>available subapplications</div>
+                              <br />
+                              <Link to="/rick"> RickAndMorty </Link>
+                              <Link to="/mush"> MushroomSearch </Link>
+                            </div>
+                          </Route>
+
+                          <Route path="/rick" component={ lazy ( () => import ( './RickAndMorty/RickAndMorty' ) ) } />
+                          <Route path="/mush" component={ lazy ( () => import ( './MushroomSearch/MushroomSearch' ) ) } />
+
+                        </Switch>
+                      </Suspense>
+                    </BrowserRouter>
                   </React.StrictMode>
-                  , document.getElementById ( 'root' ) )
+                , document.getElementById ( 'root' )
+                )
