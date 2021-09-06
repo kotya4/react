fcda421df8c30066df98a102c74ba7d54b8b5908
Sluchaynaya Kotya <sluchaynayakotya@gmail.com>
2021-09-06 17:32:53 +0300

    mushroom rewritten and become good

--- src/MushroomSearch/MushroomSearch.js
+++ src/MushroomSearch/MushroomSearch.js
@@ -1,4 +1,4 @@
-import React, { useState, Children, cloneElement, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 import styles from './MushroomSearch.module.sass'
 
@@ -6,13 +6,66 @@ import styles from './MushroomSearch.module.sass'
 
 
 
-const Grabbable = ( { ondown_with_id, children, x, y, id } ) => {
+const Grabbable = ( { children, parent, x, y } ) => {
 
-  const left = x + 'px'
-  const top = y + 'px'
+  const [ is_grabbed, is_grabbed__set ] = useState ( false )
+  const [ grabbed_x, grabbed_x__set ] = useState ( x )
+  const [ grabbed_y, grabbed_y__set ] = useState ( y )
+
+  const [ down_x_offset, down_x_offset__set ] = useState ( 0 )
+  const [ down_y_offset, down_y_offset__set ] = useState ( 0 )
+
+  const left = grabbed_x + 'px'
+  const top = grabbed_y + 'px'
+
+
+  useEffect( () => {
+
+    if ( is_grabbed ) {
+
+      parent.addEventListener ( 'mousemove', onmove )
+      parent.addEventListener ( 'mouseup', onup )
+
+    }
+
+    return () => {
+
+      parent.removeEventListener ( 'mousemove', onmove )
+      parent.removeEventListener ( 'mouseup', onup )
+
+    }
+
+
+    function onmove ( e ) {
+      e.preventDefault ()
+
+      if ( ! is_grabbed ) return
+
+      const move_x = e.clientX
+      const move_y = e.clientY
+
+      grabbed_x__set ( move_x - down_x_offset )
+      grabbed_y__set ( move_y - down_y_offset )
+
+    }
+
+
+
+    function onup ( e ) {
+      e.preventDefault ()
+
+      is_grabbed__set ( false )
+
+    }
+
+
+
+
+  }, [ parent, is_grabbed, down_x_offset, down_y_offset ] )
+
 
   return  ( <div  className={ styles.Grabbable }
-                  onMouseDown={ ondown }
+                  onMouseDown={ondown}
                   style={ { left, top } }>
 
               { children }
@@ -21,103 +74,63 @@ const Grabbable = ( { ondown_with_id, children, x, y, id } ) => {
 
 
 
+
   function ondown ( e ) {
-
-    return ondown_with_id ( id, e )
-
-  }
-
-}
-
-
-
-
-// ✳
-
-
-
-const GrabbableContainer = ( { children } ) => {
-
-  const [ down_x_offset, down_x_offset__set ] = useState ( 0 )
-  const [ down_y_offset, down_y_offset__set ] = useState ( 0 )
-
-  const [ grabbed_x, grabbed_x__set ] = useState ( 0 )
-  const [ grabbed_y, grabbed_y__set ] = useState ( 0 )
-  const [ grabbed_id, grabbed_id__set ] = useState ( null )
-
-  const ref = useRef ( null )
-
-  const elements = Children.map ( children, ( e, id ) => cloneElement ( e, { id, ondown_with_id } ) )
-
-  return  ( <div  className={ styles.GrabbableContainer }
-                  ref={ ref }
-                  onMouseMove={ onmove }
-                  onMouseUp={ onup }>
-
-              {
-                grabbed_id != null
-                  ? [ ... elements.filter ( ( _, id ) => id !== grabbed_id )
-                    , cloneElement ( elements[ grabbed_id ], {
-                        x : grabbed_x,
-                        y : grabbed_y,
-                      } )
-                    ]
-                  : elements
-              }
-
-            </div> )
-
-
-
-  function onmove ( e ) {
     e.preventDefault ()
 
-    if ( grabbed_id == null ) return
-
-    const move_x = e.clientX
-    const move_y = e.clientY
-
-    grabbed_x__set ( move_x - down_x_offset )
-    grabbed_y__set ( move_y - down_y_offset )
-
-  }
-
-
-
-  function onup ( e ) {
-    e.preventDefault ()
-
-    grabbed_id__set ( null )
-
-  }
-
-
-
-  function ondown_with_id ( id, e ) {
-    // Same as ondown but with element index passed as "id"
-
-    e.preventDefault ()
-
-    grabbed_id__set ( id )
+    is_grabbed__set ( true )
 
     const down_x = e.clientX
     const down_y = e.clientY
 
     const { x, y } = e.target.getBoundingClientRect ()
 
-    const { x:px, y:py } = ref.current.getBoundingClientRect ()
+    const { x:px, y:py } = parent.getBoundingClientRect ()
 
     down_x_offset__set ( down_x - x + px )
     down_y_offset__set ( down_y - y + py )
-
-    grabbed_x__set ( x - px )
-    grabbed_y__set ( y - py )
 
   }
 
 }
 
 
+
+
+
+const GrabbableContainer = ( { children } ) => {
+
+  const [ is_loaded, is_loaded__set ] = useState ( false )
+  const [ bounding_rect, bounding_rect__set ] = useState ( null )
+
+  const ref = useRef ( null )
+
+  useEffect( () => {
+
+    if ( ref.current ) {
+
+      is_loaded__set ( true )
+      bounding_rect__set ( ref.current.getBoundingClientRect () )
+
+    }
+
+  }, [] )
+
+
+  return  ( <div  className={ styles.GrabbableContainer }
+                  ref={ ref }>
+
+              { is_loaded
+                  ? children.map ( ( Element, idx ) => <Element key={ idx }
+                                                                parent={ ref.current }
+                                                                x={ parseInt ( Math.random () * bounding_rect.width, 10 ) }
+                                                                y={ parseInt ( Math.random () * bounding_rect.height, 10 ) } /> )
+                  : ''
+              }
+
+            </div> )
+
+}
 
 
 
@@ -127,40 +140,40 @@ const GrabbableContainer = ( { children } ) => {
 const MushroomSearch = () => {
 
   return  ( <div className={ styles.MushroomSearch }>
-
               <GrabbableContainer>
 
-                <Grabbable>
-                  <div className={ styles.Emoji }>🍄</div>
-                </Grabbable>
+                { [ props =>  <Grabbable { ...props }>
+                                <div className={ styles.Emoji }>🍄</div>
+                              </Grabbable>
 
-                <Grabbable>
-                  fuck you
-                </Grabbable>
+                  , props =>  <Grabbable { ...props }>
+                                fuck you
+                              </Grabbable>
 
-                {
-                  [ '🍄', '🍂', '🍃', '🍀', '🌿', '🍁', '☘️', '🐛', '🐜', '🐝', '🐞' ]
-                    .map ( ( e, i ) => ( <Grabbable key={ i }><div className={ styles.Emoji }>{ e }</div></Grabbable> ) )
-                }
+                  , ... [ '🍄', '🍂', '🍃', '🍀', '🌿', '🍁', '☘️', '🐛', '🐜', '🐝', '🐞' ]
+                          .map ( ( e, i ) =>
+                            props =>  <Grabbable key={ i } { ...props }>
+                                        <div className={ styles.Emoji }>{ e }</div>
+                                      </Grabbable> )
+                ] }
 
-              </GrabbableContainer>
+            </GrabbableContainer>
 
-              <div className={ styles.Emoji }>🍄 mushroom</div>
+            <div className={ styles.Emoji }>🍄 mushroom</div>
 
-              <div className={ styles.Emoji }>🍂 fallen leaf</div>
-              <div className={ styles.Emoji }>🍃 leaf fluttering in wind</div>
-              <div className={ styles.Emoji }>🍀 Four Leaf Clover</div>
-              <div className={ styles.Emoji }>🌿 Herb</div>
-              <div className={ styles.Emoji }>🍁 Maple Leaf</div>
-              <div className={ styles.Emoji }>☘️ Shamrock</div>
+            <div className={ styles.Emoji }>🍂 fallen leaf</div>
+            <div className={ styles.Emoji }>🍃 leaf fluttering in wind</div>
+            <div className={ styles.Emoji }>🍀 Four Leaf Clover</div>
+            <div className={ styles.Emoji }>🌿 Herb</div>
+            <div className={ styles.Emoji }>🍁 Maple Leaf</div>
+            <div className={ styles.Emoji }>☘️ Shamrock</div>
 
-              <div className={ styles.Emoji }>🐛 bug</div>
-              <div className={ styles.Emoji }>🐜 Ant</div>
-              <div className={ styles.Emoji }>🐝 Honeybee</div>
-              <div className={ styles.Emoji }>🐞 Lady Beetle</div>
+            <div className={ styles.Emoji }>🐛 bug</div>
+            <div className={ styles.Emoji }>🐜 Ant</div>
+            <div className={ styles.Emoji }>🐝 Honeybee</div>
+            <div className={ styles.Emoji }>🐞 Lady Beetle</div>
 
-
-            </div> )
+          </div> )
 
 }
 
---
+++ src/MushroomSearch/README.txt
@@ -0,0 +1,26 @@
+
+
+Граф этапа разработки:
+  () -> (1)
+    (1) -> делаешь что-то -> (2)
+      (2) -> синтакическая ошибка -> исправляешь ошибку -> (1)
+      (2) -> логическая ошибка -> исправляешь ошибку -> (1)
+      (2) -> внезапно все работает правильно
+                -> удивляешься
+                -> стараешься больше не трогать компонент
+
+
+
+
+TODOs:
+
+  * last touched subcomponent of GrabbableContainer not on top
+
+  * maybe useReducer is preverable over useState in GrabbableContainer
+
+
+TIPs:
+
+  * to use ModuleCSS you need to name ur css-file as [smh].module.[css|scss|sass|...]
+
+  * https://medium.com/jsdownunder/webpack-build-performance-pitfall-of-using-sass-with-css-modules-ba32f89efdcb
